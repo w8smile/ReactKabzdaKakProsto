fae0a6c3fa4506945f10e67bdac41d777b243d79
Serg Nakovnick <w8smile@gmail.com>
2023-12-04 15:05:51 +0300

    propstype

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Accordion} from "./components/acordion/Accordion";
+import {Rating} from "./components/rating/Rating";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function sum(a: number,b: number) {
+//     alert(a + b)
+// }
+// sum(23,12);
+// sum(100,300);
+// hello();
+
+function App(props: any) {
+    console.log('App rendring')
+    return <div>
+        {/*<PageTitle title={"This is APP component"}/>*/}
+        {/*<PageTitle title={"My Friends"}/>*/}
+        {/*Article 1*/}
+        {/*<Rating value={1}/>*/}
+        <Accordion titleValue={"Menu"} collapsed={false}/>
+        <Accordion titleValue={"Users"} collapsed={true}/>
+        {/*<Rating value={0}/>*/}
+        {/*<Rating value={1}/>*/}
+        {/*<Rating value={2}/>*/}
+        {/*<Rating value={3}/>*/}
+        {/*<Rating value={4}/>*/}
+        {/*<Rating value={5}/>*/}
     </div>
-  );
 }
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log('PageTitle rendring')
+    return <h1>{props.title}</h1>
+}
+
 
 export default App;
